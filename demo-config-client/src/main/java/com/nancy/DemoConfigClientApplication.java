af5e1f2cc16b56887f38258b20090c1c1c137f00
zhou.guangfeng <zhou.guangfeng@puscene.com>
2018-05-15 18:34:26 +0800

    springcloud cluster and config-server 、 config-client

---
+++ demo-config-client/src/main/java/com/nancy/DemoConfigClientApplication.java
@@ -0,0 +1,27 @@
+package com.nancy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+public class DemoConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConfigClientApplication.class, args);
+	}
+
+	@Value("${foo}")
+	String foo;
+
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return foo;
+	}
+
+}
---
+++ demo-config-server/src/main/java/com/nancy/democonfigserver/DemoConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.nancy.democonfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class DemoConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoConfigServerApplication.class, args);
+    }
+}
